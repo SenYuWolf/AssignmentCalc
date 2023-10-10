f5dce7dfd461c1a6cbcf122e853e4d77e3647992
SenYuWolf <senyuyuki739@gmail.com>
2023-10-10 14:45:25 +0800

    提交文件

--- widget.cpp
+++ widget.cpp
@@ -62,11 +62,24 @@ void Widget::onButtonGroupClicked(QAbstractButton*btn)
             else if(vec[1] == "/"){
                 vec[3] = vec[0].toFloat() / vec[2].toFloat();
             }
-            ui->lineEdit->setText(vec[3].toString());
+            else if(vec[1] == "%"){
+                vec[3] = vec[0].toInt() % vec[2].toInt();
+            }
+            else if(vec[1] == "log"){
+                vec[3] = qLn(vec[0].toFloat()) / qLn(vec[2].toFloat());
+            }
+            else if(vec[1] == "a^n"){
+                vec[3] = qPow(vec[0].toFloat(),vec[2].toFloat());
+            }
+            else if(vec[1] == "x10^n"){
+                vec[3] = vec[0].toInt() * qPow(10,vec[2].toFloat());
+            }
+            QString result = QString::number(vec[3].toFloat(), 'e', 2);
+            vec[3] = result;
+            ui->lineEdit->setText(result);
             vec[0].clear();
             vec[1].clear();
             vec[2].clear();
-            vec[3].clear();
         }
     }
     else if(name == "+")
@@ -93,6 +106,24 @@ void Widget::onButtonGroupClicked(QAbstractButton*btn)
         vec[1] = "/";
         ui->lineEdit->clear();
     }
+    else if(name == "%")
+    {
+        vec[0] = val;
+        vec[1] = "%";
+        ui->lineEdit->clear();
+    }
+    else if(name == "log")
+    {
+        vec[0] = val;
+        vec[1] = "log";
+        ui->lineEdit->clear();
+    }
+    else if(name == "a^n")
+    {
+        vec[0] = val;
+        vec[1] = "a^n";
+        ui->lineEdit->clear();
+    }
     else if(name == "MR")
     {
         ui->lineEdit->setText(vec[4].toString());
@@ -101,10 +132,46 @@ void Widget::onButtonGroupClicked(QAbstractButton*btn)
     {
         vec[4] = ui->lineEdit->text();
     }
+    else if(name == "MC")
+    {
+        vec[4].clear();
+    }
+    else if(name == "M+")
+    {
+        vec[4] = vec[4].toFloat() + ui->lineEdit->text().toFloat();
+        vec[4].toString();
+    }
+    else if(name == "M-")
+    {
+        vec[4] = vec[4].toFloat() - ui->lineEdit->text().toFloat();
+        vec[4].toString();
+    }
+    else if(name == "Ans")
+    {
+        ui->lineEdit->setText(vec[3].toString());
+    }
+    else if(name == "sin")
+    {
+        auto num = qSin(ui->lineEdit->text().toFloat());
+        QString str = QString::number(num, 'f', 2);
+        ui->lineEdit->setText(str);
+    }
+    else if(name == "cos")
+    {
+        auto num = qCos(ui->lineEdit->text().toFloat());
+        QString str = QString::number(num, 'f', 2);
+        ui->lineEdit->setText(str);
+    }
     else if(name == "+/-")
     {
         auto num = ui->lineEdit->text().toFloat();
         vec[5] = num * (-1);
         ui->lineEdit->setText(vec[5].toString());
+    }
+    else if(name == "x10^n")
+    {
+        vec[0] = val;
+        vec[1] = "x10^n";
+        ui->lineEdit->clear();
     }
 }
